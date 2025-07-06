531aa2e0dff2b847b40def8e353f6c566ac9fff2
Francisco Muro Correro <78140830+PacooMC@users.noreply.github.com>
2025-07-06 23:25:37 +0200

    Eliminar elementos decorativos superiores de Liquid Glass

--- src/components/GlassContainer.tsx
+++ src/components/GlassContainer.tsx
@@ -7,15 +7,8 @@ interface GlassContainerProps {
 
 export const GlassContainer: React.FC<GlassContainerProps> = ({ children, className = "" }) => {
   return (
-    <div className={`GlassContainer ${className}`} style={{ pointerEvents: 'none' }}>
-      <div className="GlassMaterial">
-        <div className="GlassEdgeReflection">
-          <div className="GlassRefraction">
-            <div className="GlassBlur"></div>
-          </div>
-        </div>
-      </div>
-      <div className="GlassContent" style={{ pointerEvents: 'auto' }}>
+    <div className={`GlassContainer ${className}`}>
+      <div className="GlassContent">
         {children}
       </div>
     </div>
